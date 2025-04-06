d8f75faedbdfc7109f02f7b77c9555f9b2c2ff3e
financeoptimum <finance@optimum-mobility.com>
2025-04-06 19:17:58 -0400

    fix(ci-e2e): fix bug e2e test on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    baseURL: 'http://localhost:3002', // Remplacez cela par l'URL correcte de votre application en développement
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000', // Remplacez cela par l'URL correcte de votre application en développement
   },
   reporter: [['html', { open: 'never' }]], // Génère un rapport HTML
 });
